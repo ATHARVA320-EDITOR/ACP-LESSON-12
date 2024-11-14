109e4412ee97a112e7bac88334f90acc04eb03bf
ATHARVA320-EDITOR <ddnatharva@gmail.com>
2024-11-14 16:36:36 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+shut = input("Do you want to shut down your computer? Y or N:")
+if shut == "Y":
+    print("Shut down the computer.")
+elif shut == "N":
+    print("Do not shut down the computer.")
+else:
+    print("INVALID INPUT")
+  
